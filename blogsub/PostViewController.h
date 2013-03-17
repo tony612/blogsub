64f62d8ed57775da86a0d7c72c4d0bc9cdc7de7f
tony612 <h.bing612@gmail.com>
2013-03-17 23:31:19 +0800

    add PostViewController

---
+++ blogsub/PostViewController.h
@@ -0,0 +1,16 @@
+//
+//  PostViewController.h
+//  blogsub
+//
+//  Created by Tony on 3/17/13.
+//  Copyright (c) 2013 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostViewController : UIViewController
+
+@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UITextView *content;
+
+@end
